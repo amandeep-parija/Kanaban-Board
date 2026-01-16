cb9be5095702b5b3377d5dae395655be7c87b0a7
amandeep parija <amandipparija76@gmail.com>
2026-01-16 22:48:22 +0530

    Adding Column Feature

--- main.js
+++ main.js
@@ -3,14 +3,86 @@ const progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
 
 
+let dragElement = null;
+
 const tasks = document.querySelectorAll('.task');
 
 tasks.forEach(task => {
     task.addEventListener("drag", (e) => {
         // console.log("dragging",e)
+        dragElement = task;
     })
 })
 
-progress.addEventListener("dragenter", (e) =>{
-    progress.classList.add("hover-over");
+function addDragEventsOnColumn(column){
+    column.addEventListener("dragenter", (e) =>{
+        e.preventDefault();
+        column.classList.add("hover-over");
+    });
+    column.addEventListener("dragleave", (e) =>{
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    });
+
+    column.addEventListener("dragover", (e) =>{
+        e.preventDefault();
+    });
+
+    column.addEventListener("drop", (e) =>{
+        e.preventDefault();
+        column.appendChild(dragElement);
+        column.classList.remove("hover-over");
+
+        [todo, progress, done].forEach(col =>{
+            const tasks = col.querySelectorAll(".task");
+            const count = col.querySelector(".right");
+
+            count.innerText = tasks.length;
+        })
+    });
+    
+ 
+}
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
+
+
+const toggleMOdalButton = document.querySelector('#toggle-modal');
+const modalBg = document.querySelector('.modal .bg')
+const modal = document.querySelector(".modal")
+const addTaskButton = document.querySelector("#add-new-task")
+
+
+toggleMOdalButton.addEventListener("click", () =>{
+    modal.classList.toggle("active");
+})
+
+modalBg.addEventListener("click", () =>{
+    modal.classList.remove("active");
+})
+
+
+addTaskButton.addEventListener("click", () =>{
+    const taskTitle = document.querySelector("#task-title-input").value
+    const taskDesc = document.querySelector("#task-desc-input").value
+
+    const div =document.createElement("div")
+
+    div.classList.add("task")
+    div.setAttribute("draggable", "true")
+
+    div.innerHTML = `
+    <h2>${taskTitle}</h2>
+    <p>${taskDesc}</p>
+    <button>Delete</button>
+    `
+
+    todo.appendChild(div)
+
+    div.addEventListener("drag", (e) =>{
+        dragElement = div;
+    })
+
+    modal.classList.remove("active")
 })
